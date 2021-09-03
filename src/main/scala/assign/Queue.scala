2c41c8a1c7ff5092ec1393234ff22e2629947ce0
Shubham-knoldus <shubham.shrivastava@knoldus.com>
2021-09-03 20:49:48 +0530

    OOPs Assignment 1

---
+++ src/main/scala/assign/Queue.scala
@@ -0,0 +1,17 @@
+package assign
+
+import scala.collection.mutable.ListBuffer
+
+trait Queue {
+
+  def enqueue(elementToBePushed: Int): Unit
+
+  def dequeue(queue: ListBuffer[Int]): Int = {
+
+    val removedElement = queue.head
+    queue.remove(0)
+    removedElement
+  }
+
+}
+
